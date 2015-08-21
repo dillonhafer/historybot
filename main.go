c4a779dedc2242a4193b292734f6e818f619284e
Dillon Hafer <dh@dillonhafer.com>
2015-08-21 18:15:27 -0500

    Add server flags

--- main.go
+++ main.go
@@ -2,16 +2,44 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/dillonhafer/otd"
 	"net/http"
 	"os"
 )
 
+const Version = "1.0.0"
+
+var options struct {
+	httpAddr string
+	version  bool
+}
+
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "usage:  %s [options]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.StringVar(&options.httpAddr, "http", "", "HTTP listen address (e.g. 127.0.0.1:3000)")
+	flag.BoolVar(&options.version, "version", false, "print version and exit")
+
+	flag.Parse()
+
+	if options.version {
+		fmt.Printf("historybot v%v\n", Version)
+		os.Exit(0)
+	}
 	events := otd.Events()
 
-	println("Running server")
+	serveAddress := "127.0.0.1:23000"
+	if options.httpAddr != "" {
+		serveAddress = options.httpAddr
+	}
+	fmt.Fprintln(os.Stderr, "Listening on:", serveAddress)
+	fmt.Fprintln(os.Stderr, "Use `--httpAddr` flag to change the default address")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		var jsonResp struct {
 			Text string `json:"text"`
@@ -24,7 +52,7 @@ func main() {
 		w.Write(js)
 	})
 
-	err := http.ListenAndServe("127.0.0.1:23000", nil)
+	err := http.ListenAndServe(serveAddress, nil)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
